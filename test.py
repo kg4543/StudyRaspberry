04f6489d140adb4b08a4228e12f40605ea6dd304
kg4543 <jyc351919@naver.com>
2021-07-12 15:29:39 +0900

    최초커밋

---
+++ test.py
@@ -0,0 +1,31 @@
+#val = 20
+#name = 'Kim'
+#age = val + 8
+
+#print('이름 : {0}\n 나이 : {1}'. format(name, age))
+
+# 문자열 인덱스 방법
+#greeting = 'Hello World'
+#print(greeting[0])
+#print(greeting[5:8])
+#print(greeting[:2])
+#print(greeting[:-2])
+
+# 수열 인덱스
+numbers = [0,1,2,3]
+#print(numbers)
+#print(numbers[0])
+#print(numbers[2:4])
+
+names = ['Kim','Lee','Park','Choi']
+array = numbers + names
+#print(array)
+#print(array * 2)
+
+#Tuple
+person = ('Kim', 24, 'male')
+print(person)
+print(person[1])
+
+name, age, gender = person
+print(gender)
